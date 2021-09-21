59c4c26188599b1bcdd1e52cb992e7c5658a446f
Nacho Serrano <ignaserrano@gmail.com>
2021-09-21 21:06:54 +0200

    Lectura de fichero

--- src/com/ufv/Main.java
+++ src/com/ufv/Main.java
@@ -1,5 +1,9 @@
 package com.ufv;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -10,5 +14,16 @@ public class Main {
         Persona nacho_filled = new Persona("Nacho", "Serrano", 11111111, 40);
 
         System.out.println(nacho_filled);
+        Persona persona_read;
+        InOut inOut = new InOut();
+        try {
+
+            persona_read = inOut.LecturaFichero("data.txt");
+            System.out.println(persona_read);
+        } catch (FileNotFoundException fne) {
+            System.out.println("No hemos encontrado el fichero especificado");
+        } catch (IOException ioe) {
+            System.out.println("Otro fallo no identificado.");
+        }
     }
 }
